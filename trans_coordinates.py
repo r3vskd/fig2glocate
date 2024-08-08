dac9dc13cca6fcb47f013ab97ed06259b583916d
r3vskd <44644302+r3vskd@users.noreply.github.com>
2024-08-07 23:39:04 -0600

    Update trans_coordinates.py

--- trans_coordinates.py
+++ trans_coordinates.py
@@ -16,7 +16,7 @@ def display_banner():
  Warning: It was created for educational purposes. Please don't misuse it for illegal activities. 
           ''' + Style.RESET_ALL)
 
-###################### Aqui va tu api key que debes crear en https://ipinfo.io/ ##########################
+####################### Aqui va tu api key que debes crear en https://ipinfo.io/ ##########################
 api_key = 'your_api_key'
 
 def get_ip_info(ip_address, api_key):
